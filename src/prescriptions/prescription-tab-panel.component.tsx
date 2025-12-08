086cdf07bc3d0ac32e03b00f9df606529b5f273e
Felix Kiprotich <fkiprotich845@gmail.com>
2025-12-08 16:05:17 +0300

    Initial commit

---
+++ src/prescriptions/prescription-tab-panel.component.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { MultiSelect, Search, TabPanel } from '@carbon/react';
+import { useConfig, useDebounce } from '@openmrs/esm-framework';
+import { type PharmacyConfig } from '../config-schema';
+import PrescriptionsTable from './prescriptions-table.component';
+import styles from './prescriptions.scss';
+import { useLocationForFiltering } from '../location/location.resource';
+import { type SimpleLocation } from '../types';
+
+interface PrescriptionTabPanelProps {
+  status: string;
+  isTabActive: boolean;
+}
+
+const PrescriptionTabPanel: React.FC<PrescriptionTabPanelProps> = ({ status, isTabActive }) => {
+  const { t } = useTranslation();
+  const config = useConfig<PharmacyConfig>();
+  const { filterLocations, isLoading: isFilterLocationsLoading } = useLocationForFiltering(config);
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  const [locations, setLocations] = useState<SimpleLocation[]>([]);
+
+  return (
+    <TabPanel>
+      <div className={styles.searchContainer}>
+        {config.locationBehavior?.locationFilter?.enabled &&
+          !isFilterLocationsLoading &&
+          filterLocations?.length > 1 && (
+            <MultiSelect
+              hideLabel
+              id="locationFilter"
+              label={t('filterByLocations', 'Filter by locations')}
+              items={isFilterLocationsLoading ? [] : filterLocations}
+              itemToString={(item: SimpleLocation) => item?.name}
+              onChange={({ selectedItems }) => {
+                setLocations(selectedItems);
+              }}
+              className={styles.locationFilter}
+            />
+          )}
+        <Search
+          closeButtonLabelText={t('clearSearchInput', 'Clear search input')}
+          defaultValue={searchTerm}
+          placeholder={t('searchByPatientIdOrName', 'Search by patient ID or name')}
+          labelText={t('searchByPatientIdOrName', 'Search by patient ID or name')}
+          onChange={(e) => {
+            e.preventDefault();
+            setSearchTerm(e.target.value);
+          }}
+          size="md"
+          className={styles.patientSearch}
+        />
+      </div>
+      <PrescriptionsTable
+        loadData={isTabActive}
+        status={status}
+        debouncedSearchTerm={debouncedSearchTerm}
+        locations={locations}
+      />
+    </TabPanel>
+  );
+};
+
+export default PrescriptionTabPanel;
